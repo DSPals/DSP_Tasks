0c6f2bb5414f132ed3815f2ffca09bbd7460c4bf
Nayeraneru <115358236+Nayeraneru@users.noreply.github.com>
2025-11-23 22:49:45 +0200

    Add signal comparison functions for amplitude and phase
    Added functions to compare amplitude and phase shift of signals.

--- dsp_app/utils/tests.py
+++ dsp_app/utils/tests.py
@@ -252,3 +252,34 @@ def CompareSignals_Convolution(Operation,Your_indices,Your_samples):
             print(Operation + " Test case failed, your signal have different values from the expected one") 
             return
     print(Operation + " Test case passed successfully")
+
+#Use to test the Amplitude of DFT and IDFT
+def SignalComapreAmplitude(SignalInput = [] ,SignalOutput= []):
+    if len(SignalInput) != len(SignalOutput):
+        return False
+    else:
+        for i in range(len(SignalInput)):
+            if abs(SignalInput[i]-SignalOutput[i])>0.001:
+                return False
+            elif SignalInput[i]!=SignalOutput[i]:
+                return False
+        return True
+
+def RoundPhaseShift(P):
+    while P<0:
+        p+=2*math.pi
+    return float(P%(2*math.pi))
+
+#Use to test the PhaseShift of DFT
+def SignalComaprePhaseShift(SignalInput = [] ,SignalOutput= []):
+    if len(SignalInput) != len(SignalOutput):
+        return False
+    else:
+        for i in range(len(SignalInput)):
+            A=round(SignalInput[i])
+            B=round(SignalOutput[i])
+            if abs(A-B)>0.0001:
+                return False
+            elif A!=B:
+                return False
+        return True    
