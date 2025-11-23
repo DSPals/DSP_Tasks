7cab8f43e5d6ee253529dba3a4c4a6bdcb5aba50
Nayeraneru <115358236+Nayeraneru@users.noreply.github.com>
2025-11-23 22:48:35 +0200

    Add Fourier Transform option to main menu

--- dsp_app/main_app.py
+++ dsp_app/main_app.py
@@ -3,12 +3,15 @@ from ui.operations_ui import operations_tab
 from ui.generation_ui import generation_tab
 from ui.quantization_ui import quantization_tab
 from ui.convolution_ui import Convolution_tab
+from ui.fourier_ui import fourier_tab   # <-- ADD THIS
+
 
 st.set_page_config(page_title="DSP Signal Processor", layout="wide")
 
 st.title("DSP Signal Processor")
 
-menu = st.sidebar.radio("Main Menu", ["Signal Operations", "Signal Generation", "Quantization", "Convolution"])
+menu = st.sidebar.radio("Main Menu", 
+        ["Signal Operations", "Signal Generation", "Quantization", "Convolution", "Fourier Transform"])
 display_mode = st.sidebar.selectbox("Display Mode", ["Discrete", "Continuous", "Discrete + Continuous"])
 
 if menu == "Signal Operations":
@@ -19,3 +22,6 @@ elif menu == "Quantization":
     quantization_tab(display_mode)
 elif menu == "Convolution":
     Convolution_tab(display_mode)
+elif menu == "Fourier Transform":
+    fourier_tab(display_mode)
+
